2f7d4c49127ad60cfb03211e9e6851c93f2078d5
Wes Moses <wesmoses@gmail.com>
2020-02-08 17:23:27 +0200

    Updates

---
+++ precache-manifest.0af4fd48aa7c9c8f8dfd9d0fbb8dec57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae62c9272f31f9c36b91097e1446a85",
+    "url": "/restaurants-swiper/index.html"
+  },
+  {
+    "revision": "ab31d4735c6f6a446ffe",
+    "url": "/restaurants-swiper/static/css/main.47f1c66f.chunk.css"
+  },
+  {
+    "revision": "28e987dab8b79b218bfa",
+    "url": "/restaurants-swiper/static/js/2.9c4e7995.chunk.js"
+  },
+  {
+    "revision": "a0a3063ed0e7e344881a79d4bea9f313",
+    "url": "/restaurants-swiper/static/js/2.9c4e7995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab31d4735c6f6a446ffe",
+    "url": "/restaurants-swiper/static/js/main.5e34f7ec.chunk.js"
+  },
+  {
+    "revision": "2a64ab87be598c892f4a",
+    "url": "/restaurants-swiper/static/js/runtime-main.ed5da7f2.js"
+  }
+]);
